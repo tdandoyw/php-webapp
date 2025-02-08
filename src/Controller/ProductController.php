1517ad15250a47d698b8d107bacd06a7db84beca
tdandoyw <tdandoyw@gmail.com>
2025-02-08 18:50:18 +0100

    feat: added twig bundle + product homepage

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ProductController extends AbstractController{
+    #[Route('/products', name: 'product_index')]
+    public function index(): Response
+    {
+        return $this->render('product/index.html.twig', [
+            'controller_name' => 'ProductController',
+        ]);
+    }
+}
