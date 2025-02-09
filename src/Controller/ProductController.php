938baa42a9de1e3590f5f61e2ba1112fd2c10392
tdandoyw <tdandoyw@gmail.com>
2025-02-09 18:19:15 +0100

    feat: added product data reading + display

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,13 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ProductRepository;
 
 final class ProductController extends AbstractController{
     #[Route('/products', name: 'product_index')]
-    public function index(): Response
+    public function index(ProductRepository $repository): Response
     {
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'products' => $repository->findAll(),
         ]);
     }
 }
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Product;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $product = new Product;
+        $product->setName("Product One");
+        $product->setDescription("This is the first product");
+        $product->setSize(100);
+
+        $manager->persist($product);
+
+        $product = new Product;
+        $product->setName("Product two");
+        $product->setDescription("This is the second product");
+        $product->setSize(200);
+
+        $manager->persist($product);
+
+        $manager->flush();
+    }
+}
