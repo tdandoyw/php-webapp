ca51b236c63048fc1401cf9c000d4266d94ee7a3
tdandoyw <tdandoyw@gmail.com>
2025-02-10 17:05:16 +0100

    feat: single product display

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\ProductRepository;
+use App\Repository\Product;
 
 final class ProductController extends AbstractController{
     #[Route('/products', name: 'product_index')]
@@ -13,6 +14,14 @@ final class ProductController extends AbstractController{
     {
         return $this->render('product/index.html.twig', [
             'products' => $repository->findAll(),
+        ]);
+    }
+
+    #[Route('/products/{id}', name: 'product_show')]
+    public function show(Product $product): Response
+    {
+        return $this->render('product/show.html.twig', [
+            'product' => $product
         ]);
     }
 }
