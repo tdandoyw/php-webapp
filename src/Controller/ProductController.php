96a741fd47f2297a35fedbe760c349a092c02aa3
tdandoyw <tdandoyw@gmail.com>
2025-02-13 16:36:00 +0100

    feat: added a 'new' form and page

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,7 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\ProductRepository;
-use App\Repository\Product;
+use App\Entity\Product;
+use App\Form\ProductType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class ProductController extends AbstractController{
     #[Route('/products', name: 'product_index')]
@@ -17,11 +20,38 @@ final class ProductController extends AbstractController{
         ]);
     }
 
-    #[Route('/products/{id}', name: 'product_show')]
+    #[Route('/products/{id<\d+>}', name: 'product_show')]
     public function show(Product $product): Response
     {
         return $this->render('product/show.html.twig', [
             'product' => $product
+        ]);
+    }
+
+    #[Route('/products/new', name: 'product_new')]
+    public function new(Request $request, EntityManagerInterface $manager): Response
+    {
+
+        $product = new Product;
+
+        $form = $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+
+            $manager->persist($product);
+
+            $manager->flush();
+
+            return $this->redirectToRoute('product_show', [
+                'id' => $product->getId(),
+            ]);
+
+        }
+
+        return $this->render('product/new.html.twig', [
+            'form' => $form,
         ]);
     }
 }
